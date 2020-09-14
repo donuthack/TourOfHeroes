76f3b32ba2db8af97cef114fcb88f0fae0f48158
donuthack <blueboybanana@gmail.com>
2020-09-14 18:09:57 +0300

    message + dashboard, but a little bad scss:c

---
+++ hero.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import {Observable, of} from 'rxjs';
+import{MessageService} from './message.service';
+
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+
+  constructor(public messageService: MessageService) { }
+  
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  
+
+  ngOnInit(){
+
+  }
+}
